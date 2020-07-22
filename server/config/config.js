6635747698cc6b1f59fa5cd3ec622411e0891e76
Alejandro <alexandro_sin@hotmail.com>
2020-07-22 16:34:38 -0700

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -17,7 +17,7 @@ if(process.env.NODE_ENV === 'dev')
     urlDB = 'mongodb://localhost:27017/cafe';
 }
 else{
-    urlDB= 'mongodb+srv://Alejandromex:vs9qID8sPDn3NH62@cluster0.roire.mongodb.net/cafe';
+    urlDB= process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
